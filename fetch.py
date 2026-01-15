998e861221a4cf3abd7a1afaa7a0e1ce87a9d8a0
denny0810 <59128785+denny0810@users.noreply.github.com>
2026-01-15 15:58:54 +0800

    Enable name flags and show type in fetch.py

--- fetch.py
+++ fetch.py
@@ -2,8 +2,8 @@
 # ========== User Configs Begin ==========
 # 以下是可以自定义的配置：
 STOP = False              # 暂停抓取节点
-NAME_SHOW_TYPE = False    # 在节点名称前添加如 [Vmess] 的标签
-NAME_NO_FLAGS  = False    # 将节点名称中的地区旗帜改为文本地区码
+NAME_SHOW_TYPE = True    # 在节点名称前添加如 [Vmess] 的标签
+NAME_NO_FLAGS  = True    # 将节点名称中的地区旗帜改为文本地区码
 NAME_SHOW_SRC  = False    # 在节点名称前显示所属订阅编号 (订阅见 list_result.csv)
 ABFURLS = (           # Adblock 规则黑名单
     "https://raw.githubusercontent.com/AdguardTeam/AdguardFilters/master/ChineseFilter/sections/adservers.txt",
